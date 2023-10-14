03f98315acf455d69898dde841e98d9cbd822fc0
Chase <127450469+chaseofthejungle@users.noreply.github.com>
2023-10-13 21:21:35 -0400

    Can Choose to Continue Playing or Quit Program.

--- JavaNumberGuessingGame/NumberGuessingGame.java
+++ JavaNumberGuessingGame/NumberGuessingGame.java
@@ -6,26 +6,32 @@ public class NumberGuessingGame {
         int number = 1 + (int) (100 * Math.random());
         int attempts = 5;
         int i, guess;
+        char keepPlaying = 'y';
 
         System.out.println("Guess a number between 1 to 100 accurately within 5 attempts.");
 
-        for (i = 0; i < attempts; i++) {
-            System.out.println("Enter your guess: ");
-            guess = input.nextInt();
+        while (keepPlaying == 'y' || keepPlaying == 'Y') {
+            for (i = 0; i < attempts; i++) {
+                System.out.println("Enter your guess: ");
+                guess = input.nextInt();
 
-            if (number == guess) {
-                System.out.println("Correct! It took you " + (i + 1) + " attempts to get the answer!");
-                break;
-            } else if (number > guess && i != attempts - 1) {
-                System.out.println(guess + " is too low.");
-            } else if (number < guess && i != attempts - 1) {
-                System.out.println(guess + " is too high.");
+                if (number == guess) {
+                    System.out.println("Correct! It took you " + (i + 1) + " attempts to get the answer!");
+                    break;
+                } else if (number > guess && i != attempts - 1) {
+                    System.out.println(guess + " is too low.");
+                } else if (number < guess && i != attempts - 1) {
+                    System.out.println(guess + " is too high.");
+                }
             }
-        }
 
-        if (i == attempts) {
-            System.out.println("Game Over! You ran out of attempts.");
-            System.out.println("The number was: " + number);
+            if (i == attempts) {
+                System.out.println("Game Over! You ran out of attempts.");
+                System.out.println("The number was: " + number);
+            }
+
+            System.out.println("Would you like to play again? (y = yes, n = no)");
+            keepPlaying = input.next().charAt(0);
         }
     }
 }
